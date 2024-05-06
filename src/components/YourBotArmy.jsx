765f01c36b36620daac72e8942409179c6ea8706
CharlesKibet <charlesbiegon973@gmail.com>
2024-05-06 12:35:36 +0300

    Yourbot army set up

--- src/components/YourBotArmy.jsx
+++ src/components/YourBotArmy.jsx
@@ -1,1 +1,26 @@
 //Child component
+import React from 'react'
+import BotsCard from './BotsCard'
+function YourBotArmy({bots,removeBot,deleteBot}) {
+  const botsArmy = bots.map((bot)=>{
+
+  
+  return(
+    <BotsCard
+    key={bot.id} bot={bot} 
+    clickEvent={removeBot}
+    deleteBot={deleteBot}
+    />
+  );
+})
+
+  return (
+    < div className='bg-lime-300 rounded-md'>
+    <div className="grid grid-cols-4 gap-4 my-10 ">
+{botsArmy}
+    </div>
+    </div>
+  )
+}
+
+export default YourBotArmy;
