87b0126d0233967bbec18232799f4829454cab7b
CharlesKibet <charlesbiegon973@gmail.com>
2024-05-06 12:29:46 +0300

    Parent component set up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//Main app
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <main className='px-10'>
+
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/BotsPage.jsx
@@ -0,0 +1,13 @@
+//Parent component --> I want this parent component to have two children components
+//This is where i will also have to fetch my data
+
+
+function BotsPage() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default BotsPage;
