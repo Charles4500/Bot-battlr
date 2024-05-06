2bc17e31b96bea1ac4a87d6e642244d7d4ccfeca
CharlesKibet <charlesbiegon973@gmail.com>
2024-05-06 12:44:30 +0300

    App set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 //Main app
 import React from 'react'
+import BotsPage from './components/BotsPage'
 
 function App() {
   return (
     <div>
       <main className='px-10'>
-
+<BotsPage/>
       </main>
     </div>
   )
