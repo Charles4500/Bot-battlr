369940ddd7151d91f04f42faf0dc1a78ea2fc44a
CharlesKibet <charlesbiegon973@gmail.com>
2024-05-06 12:36:19 +0300

    Botcollection setup

--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -1,2 +1,19 @@
 //Child component --> For the BotPage parent
 //Parent to the BotsCard
+import React from 'react';
+import BotsCard from './BotsCard';
+function BotsCollection({ bots, enlistBot, deleteBot }) {
+  const botItem = bots.map((bot) => {
+    return <BotsCard key={bot.id} bot={bot} 
+   
+    clickEvent={enlistBot}
+    deleteBot={deleteBot}
+    />;
+  });
+  return (
+   
+      <div className="grid grid-cols-4 gap-4 my-10 ">{botItem}</div>
+    
+  );
+}
+export default BotsCollection;
